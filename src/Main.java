e0e1eaab9831686e774d90c5a8ac92923c1e5735
Konstantin Chigridov <werest54@rambler.ru>
2023-01-23 18:23:20 +0300

    Реализуйте два класса Main и StreamMain

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = intList.stream();
-        stream.filter(x -> x > 0).filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(System.out::println);
+        List<Integer> resultList = new ArrayList<>();
+        for (Integer item : intList) {
+            if ((item > 0) && (item % 2 == 0)) {
+                resultList.add(item);
+            }
+        }
+        Collections.sort(resultList);
+        resultList.forEach(System.out::println);
     }
 }
